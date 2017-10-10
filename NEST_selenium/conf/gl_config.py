8d4838c157cd7e96c2162f6e3f268885b2f86f3a
wenbo.zeng <wenbo.zeng@counect.com>
2017-10-10 11:18:46 +0800

    ADD

--- NEST_selenium/conf/gl_config.py
+++ NEST_selenium/conf/gl_config.py
@@ -8,18 +8,19 @@ case_pic_path = "D:\\JiaoBenTest\\NEST_selenium\\fail_case_pic\\"
 card_file = "D:\\GITProjects\\OneMore\\NEST_selenium\\test_data\\batch_add_tourist_file\\c_hita_card_sample.xlsx"
 
 login_addr = "http://hita-test.counect.com/nest/index.php"
+# login_addr = "http://nestsat.shkf.counect.com/nest/index.php"
 
 admin_login_name = "admin"
 admin_login_passwd = "123456"
 
-agent_login_name = "yshxa"
+agent_login_name = "selenium_age"
 agent_login_passwd = "123456"
 
 
-merchant_login_name = "095merchant"
+merchant_login_name = "selenium_mer"
 merchant_login_passwd = "123456"
 
-llf_login_name = "095llf"
+llf_login_name = "selenium_llf"
 llf_login_passwd = "123456"
 
 
