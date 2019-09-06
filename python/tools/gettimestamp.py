7f415019bf7c2f23f6d0208f6987ca8088627bf8
zengwenbo <975102416@qq.com>
2019-09-06 16:32:11 +0800

    add

---
+++ python/tools/gettimestamp.py
@@ -0,0 +1,6 @@
+﻿# coding: utf-8
+
+import time
+
+def get_timestamp():
+	return str(time.time()).split(".")[0]
---
+++ python/tools/key_value_sign.py
@@ -0,0 +1,15 @@
+﻿# coding: utf-8
+
+import md5, operator
+
+def get_key_value_sign(pars, k):
+	''' 值排序，键值KEY签名 '''
+	sig_str = ""
+	pars = sorted(pars.items(), key = operator.itemgetter(0))
+	for tul in pars:
+		sig_str += tul[0] + "=" + tul[1] + "&"
+	sig_str = sig_str[:-1] + "&key=" + k
+	
+	m = md5.new()
+	m.update(sig_str)
+	return m.hexdigest().upper()
